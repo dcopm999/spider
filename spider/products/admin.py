a1b94a29ff6bb70d891eeefda7c092da42537db3
Pavel Tanchev <dcopm999@gmail.com>
2020-10-02 15:29:29 +0500

    Products
    * spider.products.models: Added Category model
    * spider.products.admin: Added CategoryAdmin
    
    * spider.products.models: Added Company model
    * spider.products.admin: Added CompanyAdmin
    
    * spider.products.models: Added Product model
    * spider.products.admin: Added ProductAdmin

--- spider/products/admin.py
+++ spider/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from spider.products import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_dispaly = ['description', 'is_active']
--- spider/products/models.py
+++ spider/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+
+class Company(models.Model):
+    description = models.CharField(max_length=250, verbose_name=_('Description'))
+    is_active = models.BooleanField(default=False, verbose_name=('Active'))
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name=_('Company')
+        verbose_name_plural= _('Companies')
