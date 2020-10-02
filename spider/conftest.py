c8db95e980ec94d8650f1dd2587d85c33edac3a8
Pavel Tanchev <dcopm999@gmail.com>
2020-10-02 14:02:49 +0500

    first commit

---
+++ spider/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from spider.users.models import User
+from spider.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
